2928b290525305fa4b7cfac036e60383f239b8a5
pradeep0830 <0830pradeep@gmail.com>
2017-01-13 18:40:40 -0800

    flickerimagesearch

---
+++ main.js
@@ -0,0 +1,40 @@
+// Your code here!
+$(document).ready(function () {
+
+
+    $("#search").click(function () {
+
+        var searchValue = $("#searchterm").val();
+        $("img").attr("src", '');
+
+        $.ajax({
+            url: "https://api.flickr.com/services/feeds/photos_public.gne?format=json&jsoncallback=?&tags=" + searchValue,
+            method: 'GET',
+            dataType: 'jsonp',
+
+
+            success: function (data) {
+                $.each(data.items, function (i, item) {
+                    $("<img/>").attr("src", item.media.m).prependTo("#results");
+
+
+                    $("img").click(function () {
+                        var link = $(this).attr("src");
+                        $(".modal-body").html("<img src=" + link + "/>");
+                        $("#mymodal").modal();
+
+                    });
+                })
+
+            }
+
+        });
+
+    })
+
+});
+
+
+
+
+
